90bf96d5d10497f5cc411cf6ce5bbc0f91fbecc1
ItheNagendra <nagendranaikwade2@gmail.com>
2024-11-26 13:03:03 +0530

    dzvdfzv

---
+++ src/main/java/day6/jsonScemaValidation.java
@@ -0,0 +1,10 @@
+package day6;
+
+import org.testng.annotations.Test;
+
+
+public class jsonScemaValidation {
+  @Test
+  public void f() {
+  }
+}
---
+++ src/main/java/day6/jsonSchemaValidation.java
@@ -0,0 +1,9 @@
+package day6;
+
+import org.testng.annotations.Test;
+
+public class jsonSchemaValidation {
+  @Test
+  public void f() {
+  }
+}
